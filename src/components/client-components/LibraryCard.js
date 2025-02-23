d61d67540528ea9e3e1babbe17a8c343a39deadd
nkokor <nkokor@icloud.com>
2025-02-23 15:03:02 +0100

    libraries page layout

--- src/components/client-components/LibraryCard.js
+++ src/components/client-components/LibraryCard.js
@@ -11,14 +11,23 @@ export default function LibraryCard({ library, onSelectLibrary }) {
           <p className="library-title" onClick={onSelectLibrary} style={{ cursor: "pointer" }}>
             {library.name}
           </p>
-          <p>{library.address}</p>
-          <p>{library.phone}</p>
-          <p
-            className="library-email"
-            onClick={() => window.location.href = `mailto:${library.email}`}
-          >
-            {library.email}
-          </p>
+          <div className="library-info-row">
+            <img src="/images/location.png" />
+            <p>{library.address}</p>
+          </div>
+
+          <div className="library-info-row">
+            <img src="/images/phone.png" />
+            <p>{library.phone}</p>
+          </div>
+          
+          <div className="library-info-row">
+            <img src="/images/email.png" />
+            <a href={`mailto:${library.email}`} className="text-blue-500 underline">
+              {library.email}
+            </a>
+          </div>
+
         </div>
       </div>
       <hr />
