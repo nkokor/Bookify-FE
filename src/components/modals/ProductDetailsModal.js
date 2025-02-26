1bc6c8c3e85d1b68987ea3ad96e8783593b5fdab
nkokor <nkokor@icloud.com>
2025-02-26 23:55:06 +0100

    add Check availability option on ProductDetailsModal

--- src/components/modals/ProductDetailsModal.js
+++ src/components/modals/ProductDetailsModal.js
@@ -1,10 +1,30 @@
 import React, { useState } from 'react';
 import '../../App.css';
 import '../../css/ProductDetails.css';
-import { makeReservation } from '../../api/BooksApi';
-import StatusMessageModal from './StatusMessageModal';
+import { getProductAvailability } from '../../api/BooksApi';
+import ProductAvailabilityModal from './ProductAvailabilityModal';
+import { availability } from '../../data/availability';
 
 const ProductDetailsModal = ({ onClose, product, onReserve }) => {
+  const [isAvailabilityModalOpen, setIsAvailabilityModalOpen] = useState(false);
+  const [availabilityData, setAvailabilityData] = useState(null);
+
+  const handleCheckAvailability = async () => {
+    try {
+      const data = await getProductAvailability(product.id);
+      setAvailabilityData(data);
+      setIsAvailabilityModalOpen(true);
+    } catch (error) {
+      console.error('Error checking availability:', error);
+      setAvailabilityData(availability);
+      setIsAvailabilityModalOpen(true);
+    }
+  };
+
+  const handleCloseAvailabilityModal = () => {
+    setIsAvailabilityModalOpen(false);
+  };
+
   return (
     <div className="modal-overlay">
       <div className="modal-content" id="product-details-modal">
@@ -15,34 +35,30 @@ const ProductDetailsModal = ({ onClose, product, onReserve }) => {
             <img className="details-image" src={product.coverImage} alt={product.title} />
           </div>
           <div className='info-div'>
-            <div className='detail-div'>
-              <p className='detail-title'>TITLE: </p>
-              <p className='value-p'>{product.title}</p>
-            </div>
-            <div className='detail-div'>
-              <p className='detail-title'>AUTHOR: </p>
-              <p className='value-p'>{product.author}</p>
-            </div>
-            <div className='detail-div'>
-              <p className='detail-title'>GENRE: </p>
-              <p className='value-p'>{product.genre}</p>
-            </div>
-            <div className='detail-div'>
-              <p className='detail-title'>NUMBER OF PAGES: </p>
-              <p className='value-p'>{product.numberOfPages}</p>
-            </div>
-            <div className='detail-div'>
-              <p className='detail-title'>DESCRIPTION: </p>
-            </div>
-            <div className='description-container'>
-              <p className='value-p description-p'>{product.description}</p>
-            </div>
+            <div className='detail-div'><p className='detail-title'>TITLE: </p><p className='value-p'>{product.title}</p></div>
+            <div className='detail-div'><p className='detail-title'>AUTHOR: </p><p className='value-p'>{product.author}</p></div>
+            <div className='detail-div'><p className='detail-title'>GENRE: </p><p className='value-p'>{product.genre}</p></div>
+            <div className='detail-div'><p className='detail-title'>NUMBER OF PAGES: </p><p className='value-p'>{product.numberOfPages}</p></div>
+            <div className='detail-div'><p className='detail-title'>DESCRIPTION: </p></div>
+            <div className='description-container'><p className='value-p description-p'>{product.description}</p></div>
           </div>
         </div>
-        <div onClick={() => onReserve(product)} className='reservation-button-container'>
-          <p>RESERVE</p>
+        <div className='modal-buttons'>
+          <div onClick={() => onReserve(product)} className='reservation-button-container'>
+            <p>RESERVE</p>
+          </div>
+          <div onClick={handleCheckAvailability} className='availability-button-container'>
+            <img src="/images/location.png" />
+            <p>Check availability</p>
+          </div>
         </div>
       </div>
+      {isAvailabilityModalOpen && (
+        <ProductAvailabilityModal 
+          onClose={handleCloseAvailabilityModal} 
+          availability={availabilityData} 
+        />
+      )}
     </div>
   );
 };
---
+++ src/data/availability.js
@@ -0,0 +1,26 @@
+export const availability = [
+  {
+    id: 1,
+    name: "Nacionalna i univerzitetska biblioteka Bosne i Hercegovine",
+    address: "Zmaja od Bosne 8b, 71000 Sarajevo",
+    phone: "+387 33 275 400",
+    email: "info@nub.ba",
+    image: "/images/libraries/library1.webp",
+  },
+  {
+    id: 2,
+    name: "Biblioteka Sarajevo",
+    address: "Radićeva 7, 71000 Sarajevo",
+    phone: "+387 33 238 106",
+    email: "info@biblioteka.ba",
+    image: "/images/libraries/library2.webp",
+  },
+  {
+    id: 3,
+    name: "Biblioteka Fakulteta elektrotehnike Sarajevo",
+    address: "Zmaja od Bosne bb, 71000 Sarajevo",
+    phone: "+387 33 250 700",
+    email: "biblioteka@etf.unsa.ba",
+    image: "/images/libraries/library3.webp",
+  }
+];
